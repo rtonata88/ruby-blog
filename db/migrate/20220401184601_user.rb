71c7be8c7fc4fc430dcf5266a868ab2a775b8f34
Richard Tonata <rtonata@gmail.com>
2022-04-01 21:42:00 +0200

    Add posts, users, comments, and likes migrations

---
+++ db/migrate/20220401184601_user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.integer :user_id, :primary_key
+      t.string :name
+      t.string :photo
+      t.text :bio
+      t.timestamps
+      t.integer :posts_counter
+    end
+  end
+end
---
+++ db/migrate/20220401184629_comment.rb
@@ -0,0 +1,8 @@
+class Comment < ActiveRecord::Migration[7.0]
+  def change
+     create_table :comments do |t|
+      t.text :text
+      t.timestamps
+    end
+  end
+end
