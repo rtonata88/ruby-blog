d7672d1b6b749e1de0e70fb590b10fd4f2d2f121
Richard Tonata <rtonata@gmail.com>
2022-04-01 22:10:03 +0200

    Add references to likes and comments of posts and users

--- db/migrate/20220401184601_user.rb
+++ db/migrate/20220401184601_user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.integer :user_id, :primary_key
       t.string :name
       t.string :photo
       t.text :bio
