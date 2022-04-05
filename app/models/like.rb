0cd38e9f80af6ee7894f363379a1e1b0c7530d17
Richard Tonata <rtonata@gmail.com>
2022-04-05 20:36:39 +0200

    Fixed linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-    belongs_to :posts
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :posts
 
-    after_save :update_likes_counter
-    def update_likes_counter
-        post.update(likes_counter: post.likes.count)
-    end
+  after_save :update_likes_counter
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
