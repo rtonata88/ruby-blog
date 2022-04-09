507e7709a431d7bee562568a2afed9d84fa9c657
Richard Tonata <rtonata@gmail.com>
2022-04-09 20:58:23 +0200

    Add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,5 @@
-class PostsController < ApplicationController
-  def index
-    @user = User.includes(:comments, :posts).find(params[:user_id])
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
- def new
-    @user = User.find(params[:user_id])
-    @post = Post.new
-    respond_to do |format|
-      format.html { render :new, locals: { post: @post } }
-    end
-  end
-
-  def create 
+class CommentsController < ApplicationController
+  def create
     user_id = params.require(:post).permit(:user_id)
     @user = User.where(id: user_id['user_id']).first
     @post = Post.new(params.require(:post).permit(:title, :text, :user_id))
