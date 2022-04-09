54091ec93f8938163a516485ce0fc2ab1f29e5a2
Richard Tonata <rtonata@gmail.com>
2022-04-09 22:09:47 +0200

    Add post model validations for title, likes_counter and comments_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :likes
   has_many :comments
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
