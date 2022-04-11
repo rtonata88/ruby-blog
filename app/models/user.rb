d86108a892734b5f2630b08de0617c3b678b982c
Richard Tonata <rtonata@gmail.com>
2022-04-11 23:15:24 +0200

    Add basic authentication with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+
+  def init
+      self.posts_counter = 0
+  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :comments
   has_many :posts
   has_many :likes
